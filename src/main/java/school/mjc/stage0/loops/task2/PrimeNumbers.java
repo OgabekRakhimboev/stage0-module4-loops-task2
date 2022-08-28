cce73f3f71a48d218bf60fe6c42112fbb29a964a
OgabekRakhimboev <rakhimboevogabek01@gmail.com>
2022-08-28 19:05:57 +0500

    task 4

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,24 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
-    public void printPrimeNumbers(int printToInclusive) {
+    public static void main(String[] args) {
+        printPrimeNumbers(100);
+    }
+    public static void printPrimeNumbers(int printToInclusive) {
+        int counter = 0;
+        while (counter<=printToInclusive){
+            int check = 1;
+            int count = 0;
+            while (check<=counter){
+                if (counter%check==0){
+                    count++;
+                }
+                check++;
+            }
+            if (count == 2 ){
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
